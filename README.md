c2b57b618aa5ddc7f73a78259d2b9fc6bed4090d
cuibing <kuibono@gmail.com>
2017-03-23 23:04:13 +0800

    README.md

--- README.md
+++ README.md
@@ -1,24 +1,35 @@
-# Win32Operator
+﻿# Win32Operator
 a stupid win32 operator base on user32 api
 
 
 using like following code:
 
+            for(int i=0;i<100;i++)
+            {
+                var mainWin = new Win("JpWin32").Active();
 
-var mainWin = new Win("JpWin32").Active();
+                var pn = mainWin.Child("#32770");
+                pn.Child("Edit").SetText("喜連瓜破");
+                pn.Child("Edit", null, 1).SetText("福");
+                var NewWinPrt = pn.Child("Button", "平均経路の探索(&X)").Click();
 
-            var pn = mainWin.Child("#32770");
-            pn.Child("Edit").SetText("喜連瓜破");
-            pn.Child("Edit",null,1).SetText("福");
-            var NewWinPrt = pn.Child("Button", "平均経路の探索(&X)").Click();
+                var newWin1 = mainWin.GetnewWindow("#32770").Print();
+                var newWin2 = mainWin.GetnewWindow("#32770", "経路の印刷").Child("Button", "OK").Click();
 
-            var newWin1 = mainWin.GetnewWindow(mainWin.allWindowPtrs, "#32770").Print();
-            var newWin2 = mainWin.GetnewWindow(mainWin.allWindowPtrs, "#32770").Child("Button", "OK").Click();
-            var newWin = mainWin.GetnewWindow(mainWin.allWindowPtrs, "#32770");
+                Console.WriteLine("start find new window");
+                var newWin = mainWin.GetnewWindow("#32770", "Save Print Output As");
+                Console.WriteLine(newWin.Name);
 
-            newWin.Child(null, null, 0)
-                .Child(null, null, 0)
-                .Child(null, null, 0)
-                .Child(null, null, 0)
-                .Child("Edit", null, 0)
-                .SetText("asp.net");
+                Thread.Sleep(5000);
+                var edit = newWin
+                    .Child("DUIViewWndClassName")
+                    .Child("DirectUIHWND")
+                    .Child("FloatNotifySink")
+                    .Child("ComboBox")
+                    .Child("Edit")
+                    .SetText(DateTime.Now.ToString("yyyyMMddHHmmssfff"));
+                newWin.Child("Button", "&Save").Click();
+
+                new Win("#32770", "印刷中...").WaitWindowDispose();
+                newWin1.Close();
+            }
--- Win32API/WindowExtention/WindowOperation.cs
+++ Win32API/WindowExtention/WindowOperation.cs
@@ -3,33 +3,63 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Win32.WindowExtention
 {
     public static class WindowOperation
     {
-        private static int timeOut = 5000;
+        private static long timeOut = 5000;
 
         public static Win SetText(this Win win, string text)
         {
-            win.Success = User32.SendMessage((IntPtr)win.Handle, 0x000C, text.Length, text) > 0;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            do
+            {
+                win.Success = User32.SendMessage((IntPtr)win.Handle, 0x000C, text.Length, text) > 0;
+            }
+            while (!win.Success && sw.ElapsedMilliseconds < timeOut);
             return win;
         }
 
         public static Win Click(this Win win)
         {
-            win.Success = User32.SendMessage((IntPtr)win.Handle, 0x00F5, 0, 0) > 0;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            do
+            {
+                win.Success = User32.SendMessage((IntPtr)win.Handle, 0x00F5, 0, 0) == 0;
+                
+            }
+            while (!win.Success && sw.ElapsedMilliseconds < timeOut);
+           
             return win;
         }
         public static Win Command(this Win win, int wMsg)
         {
-            win.Success = User32.PostMessage((IntPtr)win.Handle, 0x0111, wMsg, 0) > 0;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            do
+            {
+                win.Success = User32.PostMessage((IntPtr)win.Handle, 0x0111, wMsg, 0) > 0;
+            }
+            while (!win.Success && sw.ElapsedMilliseconds < timeOut);
+           
             return win;
         }
         public static Win Close(this Win win)
         {
-            win.Success = User32.SendMessage((IntPtr)win.Handle, 0x0010, 0, 0) > 0;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            do
+            {
+                win.Success = User32.SendMessage((IntPtr)win.Handle, 0x0010, 0, 0) == 0;
+            }
+            while (!win.Success && sw.ElapsedMilliseconds < timeOut);
+
+            
             return win;
         }
 
@@ -39,30 +69,57 @@ namespace Win32.WindowExtention
         }
         public static Win Active(this Win win)
         {
-            win.Success = User32.SetActiveWindow((IntPtr)win.Handle) > 0;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            do
+            {
+                win.Success = User32.SetActiveWindow((IntPtr)win.Handle) > 0;
+            }
+            while (!win.Success && sw.ElapsedMilliseconds < timeOut);
+           
             return win;
         }
 
+        public static void WaitWindowDispose(this Win win)
+        {
+            var disposed = false;
+            while(disposed==false)
+            {
+                var w = new Win() { Handle = win.Handle };
+                if(string.IsNullOrEmpty(w.Name) && string.IsNullOrEmpty(w.Class))
+                {
+                    disposed = true;
+                }
+                Thread.Sleep(200);
+            }
+        }
        
-        public static Win GetnewWindow(this Win win, List<int> oldPtrs, string cls)
+        public static Win GetnewWindow(this Win win, string cls,string name="")
         {
 
             Stopwatch sw = new Stopwatch();
             sw.Start();
-
-            while (sw.ElapsedMilliseconds < timeOut)
+            List<int> oldPtrs = win.allWindowPtrs;
+            do
             {
                 win.GetAllWindows();
                 var diffWindows = win.allWindowPtrs.Where(x => oldPtrs.Contains(x) == false).Select(x => new Win { Handle = x });
+                Console.WriteLine(string.Format("different windows count:{0}", diffWindows.Count()));
+                diffWindows.ToList().ForEach(x =>
+                {
+                    Console.WriteLine(string.Format("Handle:{0} cls:{1} name:{2}", x.Handle, x.Class, x.Name));
+                });
                 if (string.IsNullOrEmpty(cls) == false)
                 {
-                    diffWindows = diffWindows.Where(x => x.Class == cls);
+                    diffWindows = diffWindows.Where(x => x.Class.Contains(cls) && x.Name.Contains(name));
                 }
                 if (diffWindows.Count() > 0)
                 {
                     return diffWindows.First();
                 }
+                Thread.Sleep(200);
             }
+            while (sw.ElapsedMilliseconds < timeOut);
             return null;
         }
     }
